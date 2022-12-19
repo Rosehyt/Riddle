b7bc361ee605bfeb42b86d6822ab09ec5dd6eba3
Rosehyt <p055362703@gmail.com>
2022-12-20 02:32:22 +0800

    make it better

--- reiddle_sv.py
+++ reiddle_sv.py
@@ -8,29 +8,50 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 app = Flask(__name__)
+
 @app.route("/webhook3", methods=["POST"])
 def webhook3():
     req = request.get_json(force=True)
     action =  req.get("queryResult").get("action")
+    session = req.get("session")[-12:-1]  #取最後12個字元
+
     if (action == "keywordchoice"):
         keyword =  req.get("queryResult").get("parameters").get("keyword")
-        guessnum = int(data.get("queryResult").get("parameters").get("any"))
-
+       
         collection_ref = db.collection("riddle")
         docs = collection_ref.get()
         dict = doc.to_dict()
 
         if(keyword==dict["item"]):
+            n = request.form["keyword"]
+        
+            db = firestore.client()
+            collection_ref = db.collection("item")
+            docs = collection_ref.get()
+        
             result =""
 
             for doc in docs:
                 dict = doc.to_dict()
-                if keyword in dict["num"]:
-                    result = "問題 : <br>"+format(dict["Question"])+"<br>"+"答案 : <br>"+format(dict["Answer"])+"<br>"+"解釋 : <br>"+format(dict["Explanation"])+"<br>"
+                if n in dict["num"]:
+                    info = "問題"+ n +" : "+format(dict["Question"])+"\n"+"答案 : \n"+format(dict["Answer"])+"\n"+"解釋 : \n"+format(dict["Explanation"])+"\n"
+                    return make_response(jsonify({"fulfillmentText": info}))
+            # if result == "":
+            #     result = "是怎樣?皮啊?給我重輸"
+            #     return result
+            # else:
+            #     return render_template("riddle.html")
 
-            if result == "":
-                result = "是怎樣?皮啊?給我重輸"
-            return result
+            # result =""
+
+            # for doc in docs:
+            #     dict = doc.to_dict()
+            #     if keyword in dict["num"]:
+            #         result = "問題 : <br>"+format(dict["Question"])+"<br>"+"答案 : <br>"+format(dict["Answer"])+"<br>"+"解釋 : <br>"+format(dict["Explanation"])+"<br>"
+
+            # if result == "":
+            #     result = "是怎樣?皮啊?給我重輸"
+            # return result
             
            
  '''      elif(keyword==dict["place"]):
