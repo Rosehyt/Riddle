67cab7e14fdad4f1e542a81c86ca974051d1c7f7
Rosehyt <p055362703@gmail.com>
2022-12-21 07:52:28 +0800

    make it better

--- reiddle_sv.py
+++ reiddle_sv.py
@@ -18,63 +18,49 @@ def index():
     homepage += "<a href=/random>隨機型謎語</a><br>"
     return homepage
 
+import random
+from flask import Flask, render_template, request, make_response, jsonify
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	homepage = "<h1>猜謎語</h1>"
+	homepage += "<a href=/riddle>物品型謎語</a><br>"
+	homepage += "<a href=/place>地方型謎語</a><br>"
+	homepage += "<a href=/slang>俚語型謎語</a><br>"
+	homepage += "<a href=/random>隨機型謎語</a><br>"
+	return homepage
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    # build a request object
-    req = request.get_json(force=True)
-    # fetch queryResult from json
-    action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "動作：" + action + "； 查詢內容：" + msg
-    return make_response(jsonify({"fulfillmentText": info}))
+	req = request.get_json(force=True)
+	action = req.get("queryResult").get("action")
+	session = req.get("session")[-12:-1]  #取最後12個字元
 
-# @app.route("/webhook", methods=["POST"])
-# def webhook():
-#     req = request.get_json(force=True)
-#     action =  req.get("queryResult").get("action")
-#     info = action
-#     # if (action == "keywordchoice"):
-#     #     keyword =  req.get("queryResult").get("parameters").get("keyword")
-       
-#     #     result =""
+	if (action == "keywordchoice"):
+		keyword = req.get("queryResult").get("parameters").get("keyword")
 
-#     #     rate =  req.get("queryResult").get("parameters").get("keyword")
-#     #     info = "您選擇的電影分級是：" + rate
-#     return make_response(jsonify({"fulfillmentText": info}))
-
-        
-        # if(keyword=="物品"):
-
-        #     collection_ref = db.collection("item")
-        #     docs = collection_ref.get()
-            
-        #     n = random.randint(1, 10)
-        
-        #     i = docs.to_dict()["num"]
-            
-        
-        #     for doc in i:
-        #         dict = doc.to_dict() 
-        #         if n == dict["num"]:
-        #             info = "問題"+ n +" : "+format(dict["Question"])+"\n"+"答案 : \n"+format(dict["Answer"])+"\n"+"解釋 : \n"+format(dict["Explanation"])+"\n"
-        #             return jsonify({"fulfillmentText": info})
-        # else:
-        #     result = "是怎樣?皮啊?給我重輸"
-        #     return make_response(jsonify({"fulfillmentText": result}))
-            # else:
-            #     return render_template("riddle.html")
-
-            # result =""
-
-            # for doc in docs:
-            #     dict = doc.to_dict()
-            #     if keyword in dict["num"]:
-            #         result = "問題 : <br>"+format(dict["Question"])+"<br>"+"答案 : <br>"+format(dict["Answer"])+"<br>"+"解釋 : <br>"+format(dict["Explanation"])+"<br>"
-
-            # if result == "":
-            #     result = "是怎樣?皮啊?給我重輸"
-            # return result
-            
+		collection_ref = db.collection("riddle")
+		docs = collection_ref.get()
+		info =""
+		if(keyword=="物品"):
+			n = random.randint(1, 10)
+			i = docs.to_dict()["item"]
+			for doc in i:
+				dict = doc.to_dict()
+				if n == dict["num"]:
+					info = "問題"+ n +" : "+format(dict["Question"])+"\n"+"答案 : \n"+format(dict["Answer"])+"\n"+"解釋 : \n"+format(dict["Explanation"])+"\n"
+		return make_response(jsonify({"fulfillmentText": info}))
+	else:
+		result = "是怎樣?皮啊?給我重輸"
+		return make_response(jsonify({"fulfillmentText": result}))
            
     #    elif(keyword==dict["place"]):
     #         random = random.randint(1,7))
