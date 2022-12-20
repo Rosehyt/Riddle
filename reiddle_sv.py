1d1ef0a25e34b91dd6035411e60dc87d85c8f0ce
Rosehyt <p055362703@gmail.com>
2022-12-21 02:27:27 +0800

    make it better

--- reiddle_sv.py
+++ reiddle_sv.py
@@ -30,10 +30,10 @@ def webhook():
         result =""
 
         
-    if (action == "rateChoice"):
-        rate =  req.get("queryResult").get("parameters").get("keyword")
-        info = "您選擇的電影分級是：" + rate
-    return make_response(jsonify({"fulfillmentText": info}))
+        if (action == "rateChoice"):
+            rate =  req.get("queryResult").get("parameters").get("keyword")
+            info = "您選擇的電影分級是：" + rate
+        return make_response(jsonify({"fulfillmentText": info}))
 
         
         if(keyword=="物品"):
