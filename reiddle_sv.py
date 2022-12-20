dbc2642175abf47a0e17ba2fb2abcd80aa282d58
Rosehyt <p055362703@gmail.com>
2022-12-21 03:04:52 +0800

    make it better

--- reiddle_sv.py
+++ reiddle_sv.py
@@ -20,17 +20,27 @@ def index():
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
+    # build a request object
     req = request.get_json(force=True)
+    # fetch queryResult from json
     action =  req.get("queryResult").get("action")
-    info = action
-    # if (action == "keywordchoice"):
-    #     keyword =  req.get("queryResult").get("parameters").get("keyword")
-       
-    #     result =""
-
-    #     rate =  req.get("queryResult").get("parameters").get("keyword")
-    #     info = "您選擇的電影分級是：" + rate
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
     return make_response(jsonify({"fulfillmentText": info}))
+
+# @app.route("/webhook", methods=["POST"])
+# def webhook():
+#     req = request.get_json(force=True)
+#     action =  req.get("queryResult").get("action")
+#     info = action
+#     # if (action == "keywordchoice"):
+#     #     keyword =  req.get("queryResult").get("parameters").get("keyword")
+       
+#     #     result =""
+
+#     #     rate =  req.get("queryResult").get("parameters").get("keyword")
+#     #     info = "您選擇的電影分級是：" + rate
+#     return make_response(jsonify({"fulfillmentText": info}))
 
         
         # if(keyword=="物品"):
