d617e70664b35c8f3303e3da547f906428828fa2
Rosehyt <67593265+Rosehyt@users.noreply.github.com>
2022-12-21 23:23:55 +0800

    Update index.py

--- index.py
+++ index.py
@@ -43,40 +43,57 @@ def riddle():
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-	req = request.get_json(force=True)
+    req = request.get_json(force=True)
+    action =  req.get("queryResult").get("action")
+    if(action == "keywordchoice"):
+        keyword = req.get("queryResult").get("parameters").get("keyword")
+        collection_ref = db.collection("aaa")
+        docs = collection_ref.get()
+        result = ""
+        dict = doc.to_dict()
+        result += "題目：" + dict["Question"] + "\n"
+        result += "答案：" + dict["Answer"] + "\n"
+        result += "相關資料：" + dict["Explanation"] + "\n"
+        result += "連結：" + dict["Link"] + "\n"
+        info += result
+    return make_response(jsonify({"fulfillmentText": info}))
 
-# 	session = req.get("session")[-12:-1]  #取最後12個字元
+# @app.route("/webhook", methods=["POST"])
+# def webhook():
+# 	req = request.get_json(force=True)
+
+# # 	session = req.get("session")[-12:-1]  #取最後12個字元
 	
-	action =  req.get("queryResult").get("action")
-# 	msg =  req.get("queryResult").get("queryText")
-	# info = "動作：" + action + "； 查詢內容：" + msg
-	# return make_response(jsonify({"fulfillmentText": info}))
+# 	action =  req.get("queryResult").get("action")
+# # 	msg =  req.get("queryResult").get("queryText")
+# 	# info = "動作：" + action + "； 查詢內容：" + msg
+# 	# return make_response(jsonify({"fulfillmentText": info}))
 
-	if (action == "keywordchoice"):
-		keyword = req.get("queryResult").get("queryText")
-		result =""
+# 	if (action == "keywordchoice"):
+# 		keyword = req.get("queryResult").get("queryText")
+# 		result =""
 
-		if(keyword=="物品"):
-			n = random.randint(1, 10)
-			result +=n
-			collection_ref = db.collection("item")
-			docs = collection_ref.get()
-			found = False
+# 		if(keyword=="物品"):
+# 			n = random.randint(1, 10)
+# 			result +=n
+# 			collection_ref = db.collection("item")
+# 			docs = collection_ref.get()
+# 			found = False
 			
-			for doc in docs:
-				dict = doc.to_dict()
-				if (n == dict["num"]):
-					found = True
-					result = "問題 : \n" +format(dict["Question"])+"\n"+"答案 : \n"+format(dict["Answer"])+"\n"+"解釋 : \n"+format(dict["Explanation"])+"\n"
+# 			for doc in docs:
+# 				dict = doc.to_dict()
+# 				if (n == dict["num"]):
+# 					found = True
+# 					result = "問題 : \n" +format(dict["Question"])+"\n"+"答案 : \n"+format(dict["Answer"])+"\n"+"解釋 : \n"+format(dict["Explanation"])+"\n"
 
-			if not found:
-				result += "很抱歉，目前無符合這個關鍵字的相關電影喔"
+# 			if not found:
+# 				result += "很抱歉，目前無符合這個關鍵字的相關電影喔"
 
-		return make_response(jsonify({"fulfillmentText": result}))
+# 		return make_response(jsonify({"fulfillmentText": result}))
 
-	else:
-		result = "是怎樣?皮啊?給我重輸"
-		return make_response(jsonify({"fulfillmentText": result}))
+# 	else:
+# 		result = "是怎樣?皮啊?給我重輸"
+# 		return make_response(jsonify({"fulfillmentText": result}))
 	
 if __name__ == "__main__":
     app.run()
