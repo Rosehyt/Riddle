a3bffff3b88427be213c083e92d2e313d0cc937d
Rosehyt <67593265+Rosehyt@users.noreply.github.com>
2022-12-21 21:00:16 +0800

    Update index.py

--- index.py
+++ index.py
@@ -43,41 +43,41 @@ def riddle():
     	else:
         	return render_template("riddle.html")
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-	req = request.get_json(force=True)
+# @app.route("/webhook", methods=["POST"])
+# def webhook():
+# 	req = request.get_json(force=True)
 
-# 	session = req.get("session")[-12:-1]  #取最後12個字元
+# # 	session = req.get("session")[-12:-1]  #取最後12個字元
 	
-	action =  req.get("queryResult").get("action")
-# 	msg =  req.get("queryResult").get("queryText")
-	# info = "動作：" + action + "； 查詢內容：" + msg
-	# return make_response(jsonify({"fulfillmentText": info}))
+# 	action =  req.get("queryResult").get("action")
+# # 	msg =  req.get("queryResult").get("queryText")
+# 	# info = "動作：" + action + "； 查詢內容：" + msg
+# 	# return make_response(jsonify({"fulfillmentText": info}))
 
-    	if (action == "keywordchoice"):
-		keyword = req.get("queryResult").get("parameters").get("keyword")
-		result =""
+#     	if (action == "keywordchoice"):
+# 		keyword = req.get("queryResult").get("parameters").get("keyword")
+# 		result =""
 
-		if(keyword=="物品"):
-			n = random.randint(1, 10)
+# 		if(keyword=="物品"):
+# 			n = random.randint(1, 10)
 			
-            		collection_ref = db.collection("item")
-		    	docs = collection_ref.get()
-		    	found = False
+#             		collection_ref = db.collection("item")
+# 		    	docs = collection_ref.get()
+# 		    	found = False
 
-			for doc in docs:
-				dict = doc.to_dict()
-				if n == dict["num"]:
-                    			found = True
-					result = "問題 : \n" +format(dict["Question"])+"\n"+"答案 : \n"+format(dict["Answer"])+"\n"+"解釋 : \n"+format(dict["Explanation"])+"\n"
+# 			for doc in docs:
+# 				dict = doc.to_dict()
+# 				if n == dict["num"]:
+#                     			found = True
+# 					result = "問題 : \n" +format(dict["Question"])+"\n"+"答案 : \n"+format(dict["Answer"])+"\n"+"解釋 : \n"+format(dict["Explanation"])+"\n"
 
-            		if not found:
-                		result += "很抱歉，目前無符合這個關鍵字的相關電影喔"
+#             		if not found:
+#                 		result += "很抱歉，目前無符合這個關鍵字的相關電影喔"
 
-        	return make_response(jsonify({"fulfillmentText": result}))
-	else:
-		result = "是怎樣?皮啊?給我重輸"
-		return make_response(jsonify({"fulfillmentText": result}))
+#         	return make_response(jsonify({"fulfillmentText": result}))
+# 	else:
+# 		result = "是怎樣?皮啊?給我重輸"
+# 		return make_response(jsonify({"fulfillmentText": result}))
 
 if __name__ == "__main__":
     app.run()
