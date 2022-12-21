5e0f7bf2a6841b59957244b4392137ccabb019b9
Rosehyt <67593265+Rosehyt@users.noreply.github.com>
2022-12-21 15:21:58 +0800

    Update index.py

--- index.py
+++ index.py
@@ -46,8 +46,8 @@ def riddle():
 @app.route("/webhook", methods=["POST"])
 def webhook():
     	req = request.get_json(force=True)
-	action = req.get("queryResult").get("action")
-	session = req.get("session")[-12:-1]  #取最後12個字元
+# 	action = req.get("queryResult").get("action")
+# 	session = req.get("session")[-12:-1]  #取最後12個字元
 	
 	action =  req.get("queryResult").get("action")
     	msg =  req.get("queryResult").get("queryText")
